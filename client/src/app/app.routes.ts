1e15f813ae344943ea606678bdebf70e5a5b6b73
worawut-kr <worawut.kr@outlook.co.th>
2025-01-15 11:30:36 +0700

    21

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: HomeComponent
+    },
+  
+    { // Lazy Load component
+      path: 'not-found',
+      loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent)
+    },
+    {
+      path: 'login',
+      loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+    },
+    {
+      path: 'server-error',
+      loadComponent: () => import('./server-error/server-error.component').then(c => c.ServerErrorComponent)
+    }
+  ];
---
+++ client/src/app/member/member.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member',
+  imports: [],
+  templateUrl: './member.component.html',
+  styleUrl: './member.component.css'
+})
+export class MemberComponent {
+
+}

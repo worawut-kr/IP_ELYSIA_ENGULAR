f48f6061eb8596d6a0340fd80143cfd7c996bb2f
worawut-kr <worawut.kr@outlook.co.th>
2025-02-05 07:47:34 +0700

    7/8

--- AngulaarSexyMe-master/src/app/_interceptors/loading.interceptor.ts
+++ AngulaarSexyMe-master/src/app/_interceptors/loading.interceptor.ts
@@ -1,5 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core'
+import { delay, finalize } from 'rxjs'
+import { LoadingService } from '../services/loading.service';
+
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const loadingservice = inject(LoadingService)
+  loadingservice.loading()
+  return next(req).pipe(
+    delay(1000),
+    finalize(() =>
+      loadingservice.idle())
+  )
+ 
 };
--- AngulaarSexyMe-master/src/app/app.component.ts
+++ AngulaarSexyMe-master/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent,NgxSpinnerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
